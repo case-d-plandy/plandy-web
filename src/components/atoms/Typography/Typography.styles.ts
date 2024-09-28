a756e4564c7643e8a070f5f0093ec2dda8b8f785
김종혁 <kimjhs@kakao.com>
2024-09-29 01:17:05 +0900

    feat: Typography 컴포넌트 color prop 추가

--- src/components/atoms/Typography/Typography.styles.ts
+++ src/components/atoms/Typography/Typography.styles.ts
@@ -3,11 +3,19 @@ import styled from "basic-styled";
 import type { TypographyProps } from "./Typography";
 
 export const StyledTypography = styled.div<
-  Pick<TypographyProps, "variant" | "size" | "fontSize" | "fontWeight">
+  Pick<TypographyProps, "variant" | "size" | "fontSize" | "fontWeight" | "color">
 >`
-  ${({ theme: { typography }, variant = "body", size = "medium", fontSize, fontWeight }) => ({
+  ${({
+    theme: { palette, typography },
+    variant = "body",
+    size = "medium",
+    fontSize,
+    fontWeight,
+    color
+  }) => ({
     fontSize: fontSize || typography[variant][size].size,
     fontWeight: fontWeight || typography[variant][size].weight,
-    letterSpacing: typography[variant][size].letterSpacing
+    letterSpacing: typography[variant][size].letterSpacing,
+    color: palette[color as keyof typeof color] || color
   })};
 `;
--- src/components/atoms/Typography/Typography.tsx
+++ src/components/atoms/Typography/Typography.tsx
@@ -1,13 +1,18 @@
 import typography from "@theme/typography";
+import type { BasicTheme } from "basic-styled";
+import type { Properties } from "csstype";
 import { HTMLAttributes } from "react";
 
 import { StyledTypography } from "./Typography.styles";
 
-export interface TypographyProps extends HTMLAttributes<HTMLDivElement> {
+export interface TypographyProps extends Omit<HTMLAttributes<HTMLDivElement>, "color"> {
   variant?: keyof typeof typography;
   size?: keyof (typeof typography)["display"];
   fontSize?: string;
   fontWeight?: number;
+  color?:
+    | Extract<keyof BasicTheme["palette"], "primary" | "secondary" | "tertiary">
+    | Properties["color"];
 }
 
 function Typography({
