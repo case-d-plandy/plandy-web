ccf3ead054375dcf82462bd47d44d5ad02138e34
Danah <danah.kim@kakao.com>
2024-11-23 23:12:40 +0900

    fix: asset 위치 변경 및 일본어 이미지 추가

--- src/pages/_components/Intro/Intro.tsx
+++ src/pages/_components/Intro/Intro.tsx
@@ -1,5 +1,9 @@
 import { useTranslation } from "react-i18next";
 
+import AppStoreIcon from "@assets/images/icon_app_store.png";
+import PlandyMockupEn from "@assets/images/plandy_mockup_en.png";
+import PlandyMockupJA from "@assets/images/plandy_mockup_ja.png";
+import PlandyMockupKO from "@assets/images/plandy_mockup_ko.png";
 import Box from "@components/atoms/Box";
 import Button from "@components/atoms/Button";
 import Typography from "@components/atoms/Typography";
@@ -7,6 +11,12 @@ import Typography from "@components/atoms/Typography";
 import i18n from "@utils/i18n";
 
 import { ImageBox, StyledIntro, SubTitle, Title, TitleBox } from "./Intro.styles";
+
+const PlandyMockupImage = {
+  en: PlandyMockupEn,
+  ko: PlandyMockupKO,
+  ja: PlandyMockupJA
+};
 
 function Intro() {
   const { t } = useTranslation();
@@ -19,9 +29,10 @@ function Intro() {
         <Box mt={2}>
           <Button
             variant="text"
-            startIcon={<img src="/images/icon_app_store.png" alt="App Store" width="24px" />}
+            startIcon={<img src={AppStoreIcon} alt="App Store" width="24px" />}
             onClick={() => window.open("https://apps.apple.com/us/app/id6736831438")}
           >
+            <Typography ml={1}>Download on</Typography>
             <Typography variant="title" fontWeight={500}>
               App Store
             </Typography>
@@ -30,7 +41,11 @@ function Intro() {
       </TitleBox>
       <ImageBox>
         <img
-          src={`/images/plandy_mockup_${["en", "ko"].includes(i18n.language) ? i18n.language : "en"}.png`}
+          src={
+            i18n.language.toString() in PlandyMockupImage
+              ? PlandyMockupImage[i18n.language.toString() as keyof typeof PlandyMockupImage]
+              : PlandyMockupImage.en
+          }
         />
       </ImageBox>
     </StyledIntro>
