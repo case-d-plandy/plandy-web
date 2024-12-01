1a6ce8fd141ba09b9685e13783bbf6793da14921
Danah <danah.kim@kakao.com>
2024-12-01 15:27:11 +0900

    fix: guide 페이지 임시 텍스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Error404Page from "@pages/error/404/page";
 import Error500Page from "@pages/error/500/page";
 import HowToConnectPage from "@pages/faq/how-to-connect/page";
 import FaqPage from "@pages/faq/page";
-import ConvenientSchedulingMethodPage from "@pages/guide/convenient-scheduling-method/page";
 import GuidePage from "@pages/guide/page";
 import HomePage from "@pages/page";
 import PrivacyPage from "@pages/privacy/page";
@@ -20,10 +19,6 @@ function App() {
         <Route path="/faq" element={<FaqPage />} />
         <Route path="/faq/how-to-connect" element={<HowToConnectPage />} />
         <Route path="/guide" element={<GuidePage />} />
-        <Route
-          path="/guide/convenient-scheduling-method"
-          element={<ConvenientSchedulingMethodPage />}
-        />
         <Route path="/terms" element={<TermsPage />} />
         <Route path="/privacy" element={<PrivacyPage />} />
         <Route path="/:lang" element={<LangLayout />}>
--- src/pages/guide/page.tsx
+++ src/pages/guide/page.tsx
@@ -4,7 +4,6 @@ import Typography from "@components/atoms/Typography";
 import GeneralLayout from "@components/layouts/GeneralLayout";
 import Footer from "@components/molecules/Footer";
 import Header from "@components/molecules/Header";
-import GuideList from "@pages/guide/_components/GuideList";
 import { GoogleFirebase } from "@utils/google-firebase";
 
 function GuidePage() {
@@ -17,7 +16,9 @@ function GuidePage() {
       <Typography variant="headline" fontWeight={700} mt={2} mb={2}>
         User Guide
       </Typography>
-      <GuideList />
+      <Typography fontWeight={500} mt={2} mb={2}>
+        Coming Soon !
+      </Typography>
     </GeneralLayout>
   );
 }
